c1935684073083efe6586b4cecaa44b5d2e156b8
luan santos <luan.santos@grupoicts.com.br>
2023-09-20 15:33:16 -0400

    inicializando projeto com novo template

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from "react";
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchPositionAsync, LocationAccuracy } from "expo-location"
+import { View } from "react-native";
+import { styles } from "./styles"
+import MapView, { Marker } from "react-native-maps";
+
 
 export default function App() {
+
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  const mapRef = useRef<MapView>(null);
+
+  async function requestLocationPermissionsAsync() {
+    const { granted } = await requestForegroundPermissionsAsync();
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+
+    }
+  }
+
+
+  useEffect(() => {
+    requestLocationPermissionsAsync();
+  }, [])
+
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response) => {
+      setLocation(response);
+      mapRef.current?.animateCamera({
+        pitch: 70,
+        center: response.coords,
+      })
+
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {
+        location &&
+        <MapView style={styles.map}
+        ref={mapRef}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+          }}
+        >
+
+          <Marker coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }} />
+
+
+
+        </MapView>
+      }
+
+
+
     </View>
   );
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
