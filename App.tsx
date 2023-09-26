429212bc314ecdd0234d218ea39ee5b2c2da022e
luan santos <luan.santos@grupoicts.com.br>
2023-09-25 22:19:19 -0400

    zona

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState, useRef } from "react";
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchPositionAsync, LocationAccuracy } from "expo-location"
-import { View, TouchableOpacity, Text, Image } from "react-native";
 import { styles } from "./styles"
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Polygon, Circle} from "react-native-maps";
 import { Modalize } from "react-native-modalize"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { AlwaysOpen } from "./src/components/modals/AlwaysOpen";
+import { SearchLocalByLongAndLatMapBox } from "./src/api/MapBoxApi"
+import * as geolib from 'geolib'
 
+interface Teste {
+  latitude: number, 
+  longitude: number,
+  radio: number,
+}
 
 export default function App() {
 
   const [location, setLocation] = useState<LocationObject | null>(null);
-
+  const [markerCoords, setMarkerCoords] = useState<Teste>();
+  const [markerCoordsCircle, setMarkerCoordsCicle] = useState<Teste>();
+  const [region, setRegion] = useState();
+  const [titleOnPress, settitleOnPress] = useState<string>("Local Clicado");
+  
   const mapRef = useRef<MapView>(null);
 
   async function requestLocationPermissionsAsync() {
@@ -39,9 +49,44 @@ export default function App() {
         // pitch: 70,
         center: response.coords,
       })
-
+    
     })
   }, [])
+
+
+  const handleMapPress = (event: any) => {
+    const { coordinate } = event.nativeEvent;
+    setMarkerCoords(coordinate);
+  };
+
+
+  const setLocationOnPress = async (event: any) => {
+    const { coordinate } = event.nativeEvent;
+    const search = await SearchLocalByLongAndLatMapBox(coordinate.longitude, coordinate.latitude)
+    settitleOnPress(search.features[0].place_name)
+
+    const te = [
+      { latitude:-3.0439293, longitude: -59.9253114 },
+      { latitude: -3.0441272, longitude:-59.9243227 },
+      { latitude: -3.0430551, longitude: -59.9238853 },
+  ]
+
+    const teste: Teste | any = geolib.getCenter(te);
+    const limit = geolib.getBounds(te);
+
+  console.log({limit});
+  
+  const t = geolib.getRhumbLineBearing(
+    { latitude: limit.maxLat, longitude: limit.maxLng },
+    { latitude: limit.minLat, longitude:limit.minLng }
+);
+
+  
+  setMarkerCoordsCicle({ latitude: teste.latitude, longitude: teste.longitude, radio: t / 2  })
+  };
+
+
+  
 
   const modalizeRef = useRef<Modalize>(null);
   function onOpen() {
@@ -49,24 +94,40 @@ export default function App() {
   }
   return (
     <GestureHandlerRootView style={styles.container}>
-
-
+      
       {
         location &&
         <MapView style={styles.map}
           ref={mapRef}
+          onPress={handleMapPress}
+          region={region}
+          zoomEnabled={true}
           initialRegion={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
             latitudeDelta: 0.005,
             longitudeDelta: 0.005
           }}
+          minZoomLevel={17}
+          showsUserLocation={true}
+          loadingEnabled={true}
         >
 
-          <Marker coordinate={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-          }} />
+          
+
+          
+
+          {markerCoords && (
+            <Marker coordinate={markerCoords} title={titleOnPress}  onPress={setLocationOnPress}   />
+          )}
+
+          {markerCoordsCircle && (
+            <Circle   
+            center={{ latitude: markerCoordsCircle.latitude, longitude: markerCoordsCircle.longitude }}
+            radius={markerCoordsCircle.radio}
+            fillColor={'rgba(200, 300, 200, 0.5)'}
+            />
+          )}
 
         </MapView>
       }
--- src/api/MapBoxApi.ts
+++ src/api/MapBoxApi.ts
@@ -1,14 +1,21 @@
 const ACCESS_TOKEN_MAP_BOX =
-"access_token=pk.eyJ1IjoibHVhbnM2NjA1IiwiYSI6ImNsbW5wNXppeDBoNjAycnFnZmNndnR2bmIifQ.nRh2wOdDYJXuBAkJfjH0Eg";
+  "access_token=pk.eyJ1IjoibHVhbnM2NjA1IiwiYSI6ImNsbW5wNXppeDBoNjAycnFnZmNndnR2bmIifQ.nRh2wOdDYJXuBAkJfjH0Eg";
 
 
 export const fetchLocalMapBox = (local: string) =>
-fetch(
-  `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?${ACCESS_TOKEN_MAP_BOX}`
-).then(response => response.json()).then(data => data);
+  fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?${ACCESS_TOKEN_MAP_BOX}`
+  ).then(response => response.json()).then(data => data);
 
 
 export const SearchLocalMapBox = (local: string) =>
-fetch(
-  `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?country=br&proximity=ip&types=address&language=pt&${ACCESS_TOKEN_MAP_BOX}`
-).then(response => response.json()).then(data => data);
+  fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?country=br&proximity=ip&types=address&language=pt&${ACCESS_TOKEN_MAP_BOX}`
+  ).then(response => response.json()).then(data => data);
+
+
+
+export const SearchLocalByLongAndLatMapBox = (LONGITUDE: string, LATITUDE: string) =>
+  fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${LONGITUDE},${LATITUDE}.json?${ACCESS_TOKEN_MAP_BOX}`
+  ).then(response => response.json()).then(data => data);
